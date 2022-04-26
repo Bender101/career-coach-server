54b1d1e3190ddc1815b917e7f3d5b4e4a9955449
Asafyev Sergey <91370165+Bender101@users.noreply.github.com>
2022-04-26 20:57:19 +0300

    Update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,32 +2,21 @@ const userRouter = require('express').Router()
 const { user } = require('pg/lib/defaults')
 const userController = require('../controllers/user.controller')
 
-//Вывод скилов из Skills  в компонент SelectSkills
+
 userRouter.get('/allskillsforskillsselect', userController.allSkillsForSelectSkills )
 
-//Update rate for skills
 userRouter.put('/changerate',userController.newRate)
 
-//----------------------------------------------------
 userRouter.get('/checkuserid', userController.checkUserId )
 
-
-
-//ДЛя learn 
 userRouter.post('/adduserskilllearn', userController.newUserSkillLearn )
 userRouter.get('/allUserSkillsFromLearn/:id', userController.allUserSkillsFromLearn )
-//For Skill
+
 userRouter.post('/adduserskillskill', userController.newUserSkillSkill )
 userRouter.get('/allUserSkillsFromSkills/:id', userController.allUserSkillsFromSkills )
 
-
-
-//deleteSkill
 userRouter.delete('/deleteuserskill', userController.deleteUserSkillFromSkill )
-//deleteLearn
-userRouter.delete('/deleteuserlearn', userController.deleteUserSkillFromLearn)
 
-//editSkill если время будет
-// userRouter.post('/adduserskill', userController.newUserSkill )
+userRouter.delete('/deleteuserlearn', userController.deleteUserSkillFromLearn)
 
 module.exports = userRouter
